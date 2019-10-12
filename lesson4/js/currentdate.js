312f6e879dc7164ce135feca316e956027297ead
russg20 <rus18015@byui.edu>
2019-10-12 11:05:29 -0700

    fix javascript - L04

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -5,4 +5,4 @@ var monthOpt = {month: 'long'};
 var month = currdt.toLocaleDateString('en-US', monthOpt);
 var day = currdt.getDate();
 var yr = currdt.getFullYear();
-document.getElementById("localDate").innerHTML = weekday + ", " + day + " " + month " " + yr;
+document.getElementById("localDate").innerHTML = weekday + ", " + day + " " + month + " " + yr;
