d462f6fb3bad50201a87a3446513ea4ea975e7b0
russg20 <rus18015@byui.edu>
2019-10-23 19:28:32 -0700

    add parseInt to inputs in wChill()

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,6 +1,6 @@
 function wChill() {
-    var temp = document.getElementById("hightemp").value;
-    var speed = document.getElementById("wspeed").value;
+    var temp = parseInt(document.getElementById("hightemp").value);
+    var speed = parseInt(document.getElementById("wspeed").value);
 
     if(temp<50 && speed>3) {
         var windChill = 35.74 + 0.6215*temp - 35.75*Math.pow(speed, 0.16) + 0.4275*temp*Math.pow(speed, 0.16);
