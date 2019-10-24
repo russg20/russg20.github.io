7642937651eafb1b4b04a58409f9e6bf35dd7ba6
russg20 <rus18015@byui.edu>
2019-10-24 16:28:54 -0700

    move variables out of wChill()

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,7 +1,8 @@
-function wChill() {
-    var temp = parseInt(document.getElementById("hightemp").value);
-    var speed = parseInt(document.getElementById("wspeed").value);
+var temp = parseInt(document.getElementById("hightemp").value);
+var speed = parseInt(document.getElementById("wspeed").value);
 
+function wChill() {
+    
     if(temp<50 && speed>3) {
         var windChill = 35.74 + 0.6215*temp - 35.75*Math.pow(speed, 0.16) + 0.4275*temp*Math.pow(speed, 0.16);
         document.getElementById("windchill").innerHTML = windChill + " &#8457;";
