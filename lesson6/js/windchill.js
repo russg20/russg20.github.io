0793ab3ccc62711334e5517f26c0a24191fbb2bc
russg20 <rus18015@byui.edu>
2019-10-25 18:22:42 -0700

    wChill() changes

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,8 +1,8 @@
-var temp = document.getElementById("hightemp").value;
-var speed = document.getElementById("wspeed").value;
 
 function wChill() {
-    
+    var temp = document.getElementById("hightemp").value;
+    var speed = document.getElementById("wspeed").value;
+
     if(temp<50 && speed>3) {
         var windChill = 35.74 + 0.6215*temp - 35.75*Math.pow(speed, 0.16) + 0.4275*temp*Math.pow(speed, 0.16);
         document.getElementById("windchill").innerHTML = windChill + " &#8457;";
