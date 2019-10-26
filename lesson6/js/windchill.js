f536662f0d7c56ca3e258f9db4f9b4cd7c84355c
russg20 <rus18015@byui.edu>
2019-10-26 11:06:01 -0700

    javascript windchill changes

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,15 +1,14 @@
 
-function wChill() {
-    var temp = document.getElementById("hightemp").value;
+
+    var temp = document.getElementById("hightemp").textContent;
         console.log("temp=" + temp);
-    var speed = document.getElementById("wspeed").value;
+    var speed = document.getElementById("wspeed").textContent;
         console.log("speed=" + speed);
 
-    if(temp<50 && speed>3) {
+    if(temp<=50 && speed>3) {
         var windChill = 35.74 + 0.6215*temp - 35.75*Math.pow(speed, 0.16) + 0.4275*temp*Math.pow(speed, 0.16);
             console.log("windChill=" + windChill);
         document.getElementById("windchill").innerHTML = windChill + " &#8457;";
     } else {
-        document.getElementById("windchill").innerHTML = "N/A";
+        document.getElementById("windchill").textContent = "N/A";
     }
-}
