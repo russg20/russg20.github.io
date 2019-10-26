a73e199dbfe6b48d34e4586060a3b67178d1b501
russg20 <rus18015@byui.edu>
2019-10-26 11:29:03 -0700

    add Math.round to javascript windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -6,8 +6,7 @@
         console.log("speed=" + speed);
 
     if(temp<=50 && speed>3) {
-        var windChill = 35.74 + 0.6215*temp - 35.75*Math.pow(speed, 0.16) + 0.4275*temp*Math.pow(speed, 0.16);
-            console.log("windChill=" + windChill);
+        var windChill = Math.round(35.74 + 0.6215*temp - 35.75*Math.pow(speed, 0.16) + 0.4275*temp*Math.pow(speed, 0.16));
         document.getElementById("windchill").innerHTML = windChill + " &#8457;";
     } else {
         document.getElementById("windchill").textContent = "N/A";
