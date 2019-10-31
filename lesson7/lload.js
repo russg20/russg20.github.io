425d88a48353f77b8f436e3de612c7b7f4e36139
russg20 <rus18015@byui.edu>
2019-10-30 17:50:03 -0700

    js changes for lazyload

--- lesson7/lload.js
+++ lesson7/lload.js
@@ -1,28 +1,28 @@
-const images = document.querySelectorAll("[data-src]");
+const loadImages = document.querySelectorAll("[data-src]");
 const imgOptions = {
     threshold: 0,
     rootMargin: "0px 0px -10px 0px"
-}
+};
 
-function preloadImage(img) {
+function firstLoadImg(img) {
     const src = img.getAttribute("data-src");
     if(!src) {
         return;
     }
     img.src = src;
-}
+};
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+const imgObserve = new IntersectionObserver((entries, imgObserve) => {
     entries.forEach(entry => {
         if(!entry.isIntersecting) {
             return;
         } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
+            firstLoadImg(entry.target);
+            imgObserve.unobserve(entry.target);
         }
     })
 }, imgOptions);
 
-images.forEach(image => {
-    imgObserver.observe(image);
+loadImages.forEach(image => {
+    imgObserve.observe(image);
 });
