13dc8ea035c46310dd4b7eef72459893eb95d184
russg20 <rus18015@byui.edu>
2019-11-11 19:03:29 -0800

    add javascript methods - prophets L09

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -11,10 +11,21 @@ fetch(requestURL)
         for (let i=0; i < prophets.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
+            let dob = document.createElement('p');
+            let pob = document.createElement('p');
+            let portrait = document.createElement('img');
 
             h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
             card.appendChild(h2);
+            
+            dob.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            card.appendChild(dob);
+
+            pob.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+            card.appendChild(pob);
+
+            portrait.setAttribute('src', prophets[i].imageurl);
+            portrait.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order)
 
             document.querySelector('div.cards').appendChild(card);
         }
