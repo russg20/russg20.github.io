f70ba6de1c054d27733dfcba135e72b63beb7335
russg20 <rus18015@byui.edu>
2019-11-14 16:43:40 -0800

    initial push for home page L09

---
+++ lesson9/js/gallerylazyload.js
@@ -0,0 +1,31 @@
+const loadImages = document.querySelectorAll("[data-src]");
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -10px 0px"
+};
+
+function firstLoadImg(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+    img.src = src;
+    img.onload = () => {
+        img.removeAttribute("data-src");
+    }
+};
+
+const imgObserve = new IntersectionObserver((entries, imgObserve) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        } else {
+            firstLoadImg(entry.target);
+            imgObserve.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+loadImages.forEach(image => {
+    imgObserve.observe(image);
+});
