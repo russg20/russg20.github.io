0c5073577de0b5bf08fc715f0f80282b675e947b
russg20 <rus18015@byui.edu>
2019-11-15 19:12:14 -0800

    comment out console line in js

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,7 +6,7 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject);
+        // console.table(jsonObject);
         const prophets = jsonObject['prophets'];
         for (let i=0; i < prophets.length; i++) {
             let card = document.createElement('section');
