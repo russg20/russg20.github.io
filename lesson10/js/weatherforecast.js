089c2b75419b2255140d06ca3a2798a24876108e
russg20 <rus18015@byui.edu>
2019-11-23 12:19:20 -0800

    move wind chill javascript file

--- lesson10/js/weatherforecast.js
+++ lesson10/js/weatherforecast.js
@@ -10,6 +10,19 @@ fetch(weatherURL)
         document.getElementById('hightemp').textContent = jsweatherObject.main.temp_max;
         document.getElementById('humidity').textContent = jsweatherObject.main.humidity;
         document.getElementById('wspeed').textContent = jsweatherObject.wind.speed;
+    
+        var temp = document.getElementById("hightemp").textContent;
+        //console.log("temp=" + temp);
+        var speed = document.getElementById("wspeed").textContent;
+        //console.log("speed=" + speed);
+
+        if(temp<=50 && speed>3) {
+            var windChill = Math.round(35.74 + 0.6215*temp - 35.75*Math.pow(speed, 0.16) + 0.4275*temp*Math.pow(speed, 0.16));
+            document.getElementById("windchill").innerHTML = windChill + " &#8457;";
+        } else {
+            document.getElementById("windchill").textContent = "N/A";
+        }
+    
     });
 
 fetch(forecastURL)
